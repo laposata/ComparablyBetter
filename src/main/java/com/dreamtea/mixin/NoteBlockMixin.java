4dfea888fbc16e253cc079d3c2f6b7670bb17428
Michael Laposata <laposata.m@husky.neu.edu>
2022-09-15 20:31:51 -0400

    updating version

--- src/main/java/com/dreamtea/mixin/NoteBlockMixin.java
+++ src/main/java/com/dreamtea/mixin/NoteBlockMixin.java
@@ -1,5 +1,6 @@
 package com.dreamtea.mixin;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.NoteBlock;
 import net.minecraft.util.math.BlockPos;
@@ -9,7 +10,11 @@ import org.spongepowered.asm.mixin.Mixin;
 import static com.dreamtea.events.NoteBlockPlays.noteToComparator;
 
 @Mixin(NoteBlock.class)
-public class NoteBlockMixin {
+public class NoteBlockMixin extends Block {
+
+  public NoteBlockMixin(Settings settings) {
+    super(settings);
+  }
 
   public int getComparatorOutput(BlockState state, World world, BlockPos pos) {
     return noteToComparator(state.get(NoteBlock.NOTE));
